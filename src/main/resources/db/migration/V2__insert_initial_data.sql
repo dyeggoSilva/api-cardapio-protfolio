7c8ce5f80bd14ebaaf7a917419418eaa3c9046c8
Dyeggo Bezerra <dyeggo0102@live.com>
2025-08-03 23:17:30 -0300

    Update V2__insert_initial_data.sql

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO foods (titulo, img, preco) VALUES
-('Pizza Calabresa', 'https://exemplo.com/img/pizza-calabresa.jpg', 35),
-('Hambúrguer Artesanal', 'https://exemplo.com/img/hamburguer.jpg', 25),
-('Tapioca Nordestina', 'https://exemplo.com/img/tapioca.jpg', 15);
+('Pizza Calabresa', 'https://anamariareceitas.com.br/wp-content/uploads/2024/08/pizza-de-calabresa_1723632793345-768x432.jpg', 35),
+('Hambúrguer Artesanal', 'https://www.sabornamesa.com.br/media/k2/items/cache/bf1e20a4462b71e3cc4cece2a8c96ac8_XL.jpg', 25),
+('Tapioca Nordestina', 'https://claudia.abril.com.br/wp-content/uploads/2020/02/receita-tapioca-carne-de-sol.jpg?quality=70&strip=info&w=620', 15);
