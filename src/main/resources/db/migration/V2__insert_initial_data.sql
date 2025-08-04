8fc55c6a6e42608d0b00d080a0d2af891d2ed313
Dyeggo Bezerra <dyeggo0102@live.com>
2025-08-03 22:23:43 -0300

    flyway

---
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO foods (titulo, img, preco) VALUES
+('Pizza Calabresa', 'https://exemplo.com/img/pizza-calabresa.jpg', 35),
+('Hambúrguer Artesanal', 'https://exemplo.com/img/hamburguer.jpg', 25),
+('Tapioca Nordestina', 'https://exemplo.com/img/tapioca.jpg', 15);
